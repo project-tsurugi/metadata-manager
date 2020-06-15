b086ba02e73a2f371cf0be93615af01e64941b69
Hiromasa Yano <hyano@nec.com>
2020-06-15 11:37:47 +0900

    Fix datatypes.json value

--- metadata-manager/src/datatypes.cpp
+++ metadata-manager/src/datatypes.cpp
@@ -27,114 +27,79 @@ namespace manager::metadata {
 /*
  *  @biref  initialization of DataType Metadata.
  */
-ErrorCode DataTypes::init() 
+ErrorCode DataTypes::init()
 {
     ErrorCode error = ErrorCode::UNKNOWN;
 
     ptree root;
     Metadata::init(root);
-    
+
     ptree datatypes;
     {
         ptree datatype;
-        uint64_t id = 0;
-
-        // INT : smallint
-        datatype.put(DataTypes::ID, ++id);
-        datatype.put(DataTypes::NAME, "INT");
-        datatype.put(DataTypes::PG_DATA_TYPE, 0);
-        datatype.put(DataTypes::PG_DATA_TYPE_NAME, "smallint");
-        datatypes.push_back(std::make_pair("", datatype));
 
         // INT16
-        datatype.put(DataTypes::ID, ++id);
+        datatype.put(DataTypes::ID, 2);
         datatype.put(DataTypes::NAME, "INT16");
         datatype.put(DataTypes::PG_DATA_TYPE, 0);
-        datatype.put(DataTypes::PG_DATA_TYPE_NAME, "");
+        datatype.put(DataTypes::PG_DATA_TYPE_NAME, "smallint");
+        datatype.put(DataTypes::PG_DATA_TYPE_NAME_QUALIFIED_NAME, "int2");
         datatypes.push_back(std::make_pair("", datatype));
 
-        // INT : integer
-        datatype.put(DataTypes::ID, ++id);
-        datatype.put(DataTypes::NAME, "INT");
-        datatype.put(DataTypes::PG_DATA_TYPE, 0);
-        datatype.put(DataTypes::PG_DATA_TYPE_NAME, "integer");
-        datatypes.push_back(std::make_pair("", datatype));
-
-        // INT32 : 
-        datatype.put(DataTypes::ID, ++id);
+        // INT32 :
+        datatype.put(DataTypes::ID, 4);
         datatype.put(DataTypes::NAME, "INT32");
         datatype.put(DataTypes::PG_DATA_TYPE, 0);
-        datatype.put(DataTypes::PG_DATA_TYPE_NAME, "");
-        datatypes.push_back(std::make_pair("", datatype));
-
-        // BIGINT: bigint
-        datatype.put(DataTypes::ID, ++id);
-        datatype.put(DataTypes::NAME, "BIGINT");
-        datatype.put(DataTypes::PG_DATA_TYPE, 0);
-        datatype.put(DataTypes::PG_DATA_TYPE_NAME, "bigint");
+        datatype.put(DataTypes::PG_DATA_TYPE_NAME, "integer");
+        datatype.put(DataTypes::PG_DATA_TYPE_NAME_QUALIFIED_NAME, "int4");
         datatypes.push_back(std::make_pair("", datatype));
 
         // INT64 :
-        datatype.put(DataTypes::ID, ++id);
+        datatype.put(DataTypes::ID, 6);
         datatype.put(DataTypes::NAME, "INT64");
         datatype.put(DataTypes::PG_DATA_TYPE, 0);
-        datatype.put(DataTypes::PG_DATA_TYPE_NAME, "");
+        datatype.put(DataTypes::PG_DATA_TYPE_NAME, "bigint");
+        datatype.put(DataTypes::PG_DATA_TYPE_NAME_QUALIFIED_NAME, "int8");
         datatypes.push_back(std::make_pair("", datatype));
 
-        // FLOAT : real 
-        datatype.put(DataTypes::ID, ++id);
-        datatype.put(DataTypes::NAME, "FLOAT");
-        datatype.put(DataTypes::PG_DATA_TYPE, 0);
-        datatype.put(DataTypes::PG_DATA_TYPE_NAME, "real");
-        datatypes.push_back(std::make_pair("", datatype));
-
-        // FLOAT32 : 
-        datatype.put(DataTypes::ID, ++id);
+        // FLOAT32 :
+        datatype.put(DataTypes::ID, 8);
         datatype.put(DataTypes::NAME, "FLOAT32");
         datatype.put(DataTypes::PG_DATA_TYPE, 0);
-        datatype.put(DataTypes::PG_DATA_TYPE_NAME, "");
+        datatype.put(DataTypes::PG_DATA_TYPE_NAME, "real");
+        datatype.put(DataTypes::PG_DATA_TYPE_NAME_QUALIFIED_NAME, "float4");
         datatypes.push_back(std::make_pair("", datatype));
 
-        // FLOAT64 :  
-        datatype.put(DataTypes::ID, ++id);
+        // FLOAT64 :
+        datatype.put(DataTypes::ID, 9);
         datatype.put(DataTypes::NAME, "FLOAT64");
         datatype.put(DataTypes::PG_DATA_TYPE, 0);
-        datatype.put(DataTypes::PG_DATA_TYPE_NAME, "");
-        datatypes.push_back(std::make_pair("", datatype));
-
-        // DOUBLE : double precision 
-        datatype.put(DataTypes::ID, ++id);
-        datatype.put(DataTypes::NAME, "DOUBLE");
-        datatype.put(DataTypes::PG_DATA_TYPE, 0);
         datatype.put(DataTypes::PG_DATA_TYPE_NAME, "double precision");
+        datatype.put(DataTypes::PG_DATA_TYPE_NAME_QUALIFIED_NAME, "float8");
         datatypes.push_back(std::make_pair("", datatype));
 
-        // TEXT : 
-        datatype.put(DataTypes::ID, ++id);
+        // TEXT :
+        datatype.put(DataTypes::ID, 11);
         datatype.put(DataTypes::NAME, "TEXT");
         datatype.put(DataTypes::PG_DATA_TYPE, 0);
-        datatype.put(DataTypes::PG_DATA_TYPE_NAME, "");
-        datatypes.push_back(std::make_pair("", datatype));
-
-        // STRING : text
-        datatype.put(DataTypes::ID, ++id);
-        datatype.put(DataTypes::NAME, "STRING");
-        datatype.put(DataTypes::PG_DATA_TYPE, 0);
         datatype.put(DataTypes::PG_DATA_TYPE_NAME, "text");
+        datatype.put(DataTypes::PG_DATA_TYPE_NAME_QUALIFIED_NAME, "text");
         datatypes.push_back(std::make_pair("", datatype));
 
         // CHAR : character, char
-        datatype.put(DataTypes::ID, ++id);
+        datatype.put(DataTypes::ID, 11);
         datatype.put(DataTypes::NAME, "CHAR");
         datatype.put(DataTypes::PG_DATA_TYPE, 0);
         datatype.put(DataTypes::PG_DATA_TYPE_NAME, "char");
+        datatype.put(DataTypes::PG_DATA_TYPE_NAME_QUALIFIED_NAME, "bpchar");
         datatypes.push_back(std::make_pair("", datatype));
 
         // VARCHAR : character varying, varchar
-        datatype.put(DataTypes::ID, ++id);
+        datatype.put(DataTypes::ID, 11);
         datatype.put(DataTypes::NAME, "VARCHAR");
         datatype.put(DataTypes::PG_DATA_TYPE, 0);
         datatype.put(DataTypes::PG_DATA_TYPE_NAME, "varchar");
+        datatype.put(DataTypes::PG_DATA_TYPE_NAME_QUALIFIED_NAME, "varchar");
         datatypes.push_back(std::make_pair("", datatype));
     }
     root.add_child(DataTypes::DATATYPES_NODE, datatypes);
@@ -144,9 +109,9 @@ ErrorCode DataTypes::init()
         return error;
     }
 
-    error = ErrorCode::OK;   
+    error = ErrorCode::OK;
 
-    return error;    
+    return error;
 }
 
 /**
@@ -175,4 +140,3 @@ ErrorCode DataTypes::save(
 }
 
 } // namespace manager::metadata
-
