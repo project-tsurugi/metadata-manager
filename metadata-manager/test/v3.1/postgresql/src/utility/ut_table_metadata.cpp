da8a00abbadc34c8a47c297163809b58b058fbdc
oniki <oniki.naokazu@kcc.co.jp>
2021-10-04 15:39:06 +0900

    Fixed comments in test code.

--- metadata-manager/test/v3.1/postgresql/src/utility/ut_table_metadata.cpp
+++ metadata-manager/test/v3.1/postgresql/src/utility/ut_table_metadata.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 tsurugi project.
+ * Copyright 2020-2021 tsurugi project.
  *
  * Licensed under the Apache License, version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
