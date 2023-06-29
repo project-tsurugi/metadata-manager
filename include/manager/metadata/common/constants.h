1c67963c026c7882b8af4d7ebee9652991bc8e47
oniki <oniki.naokazu@kcc.co.jp>
2023-06-29 14:06:58 +0900

    Added comment on constants.

--- include/manager/metadata/common/constants.h
+++ include/manager/metadata/common/constants.h
@@ -20,16 +20,41 @@
 
 namespace manager::metadata {
 
+/**
+ * @brief The data type of the Format Version field of the metadata.
+ */
 using FormatVersion     = std::int32_t;
 using FormatVersionType = FormatVersion;
-using Generation        = std::int64_t;
-using GenerationType    = Generation;
-using ObjectId          = std::int64_t;
-using ObjectIdType      = ObjectId;
 
+/**
+ * @brief The data type of the Generation field of the metadata.
+ */
+using Generation     = std::int64_t;
+using GenerationType = Generation;
+
+/**
+ * @brief The data type of the field indicating the object ID (e.g., table ID,
+ *   column ID) of the metadata.
+ */
+using ObjectId     = std::int64_t;
+using ObjectIdType = ObjectId;
+
+/**
+ * @brief Constants that are used for the initial and unset values of each of
+ *   the Object ID fields in the metadata.
+ */
 static constexpr const ObjectId INVALID_OBJECT_ID = -1;
+
+/**
+ * @brief Constants that are used for the initial and unset values of each of
+ *   the numeric fields in the metadata.
+ */
 static constexpr const std::int64_t INVALID_VALUE = -1;
 
+/**
+ * @brief Constants that are used for the initial and unset values of each of
+ *   the JSON string fields in the metadata.
+ */
 static constexpr const char* const kEmptyStringJson = "{}";
 
 }  // namespace manager::metadata
