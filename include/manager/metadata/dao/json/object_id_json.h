460f719dead80bba19c02243e737ee81c2132ee5
oniki <oniki.naokazu@kcc.co.jp>
2023-10-05 13:04:31 +0900

    Corrected copyright notice.

--- include/manager/metadata/dao/json/object_id_json.h
+++ include/manager/metadata/dao/json/object_id_json.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2021 tsurugi project.
+ * Copyright 2020-2021 Project Tsurugi.
  *
  * Licensed under the Apache License, generation 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/dao/json/object_id_json.cpp
+++ src/dao/json/object_id_json.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 tsurugi project.
+ * Copyright 2021 Project Tsurugi.
  *
  * Licensed under the Apache License, version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
